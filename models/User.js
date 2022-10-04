20d5fee1a546c66af57d9f22be7dbb4327146242
Christopher Hage <hage.chris@yahoo.com>
2022-10-04 16:15:29 -0300

    password hashing added

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true, min: 6, max: 50 },
   email: { type: String, required: true, min: 6, max: 100 },
@@ -8,6 +8,11 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model("User", UserSchema);
