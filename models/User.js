999ffaa84f646310d1da3cf8a276c92d1c943925
Christopher Hage <ch709979@dal.ca>
2022-10-04 15:25:32 -0300

    user schema and model created

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true, min: 6, max: 50 },
+  email: { type: String, required: true, min: 6, max: 100 },
+  password: { type: String, required: true, min: 6, max: 1024 },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model("User", UserSchema);
